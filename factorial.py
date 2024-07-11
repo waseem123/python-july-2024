5699890d9b69ac9322dbe35a31a7790a1ea98e51
WASEEM ATTAR <waseem_6957@yahoo.com>
2024-07-11 21:21:56 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,5 @@
+n = 4
+f = 1
+for i in range(1,n+1):
+    f = f + i
+print(f)
---
+++ for-loops.py
@@ -0,0 +1,15 @@
+for i in range(50):
+    print(i)
+
+mylist = ['Mobile','Headphones','Charger','Laptop']
+for i in mylist:
+    print(i)
+
+start = int(input('ENTER START VALUE - '))
+end = int(input('ENTER END VALUE - '))
+
+for i in range(start,end+1):
+    print(i)
+print("_______________")
+for i in range(1,20,2):
+    print(i)
